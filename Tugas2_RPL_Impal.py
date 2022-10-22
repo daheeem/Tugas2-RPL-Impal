a8f1b547f7fe46e4d91c5da0cd59041b35e19ee6
MUHAMMAD DAFFA IBRAHIM <84331712+daheeem@users.noreply.github.com>
2022-10-22 22:34:25 +0700

    Add files via upload

---
+++ Tugas2_RPL_Impal.py
@@ -0,0 +1,16 @@
+def exercise(comp_amount):
+	if (comp_amount == 1 or comp_amount == 2):
+		base_fee = 50
+		add_fee = 0
+	elif (comp_amount >= 3 or comp_amout <= 10):
+		base_fee = 100
+		add_fee = 10
+	elif (comp_amount > 10):
+		base_fee = 500
+		add_fee = 10
+
+	if not (servicetime(business_hours):
+		base_fee = base_fee * 2
+
+	if (drop(customer) and pick(customer)):
+		base_fee = base_fee - (base_fee * 0.5)
